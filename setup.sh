79bf489fdf7ff972ad43f33b005c6d9b6537cd17
Sai Sravan Yarlagadda <144404355+Sai-Yarlagadda@users.noreply.github.com>
2024-04-04 11:46:00 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,6 @@
+pip install pyautogui==0.9.54
+pip install time
+pip install git+https://github.com/huggingface/transformers.git@main
+pip install -q datasets
+pip install torch
+pip install transformers
